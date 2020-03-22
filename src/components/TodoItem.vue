cbb2d4044a8373569c6e0a182b2af84f4950d800
royweb <xytank@gmail.com>
2020-03-22 15:53:38 -0400

    Set up TodoItem page

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Todo Item</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "TodoItem"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,14 +3,19 @@
         <!-- v-bind to add key -->
         <!-- v-for to loop through array  -->
         <div v-bind:key="todo.id" v-for="todo in todos">
-            <h3>{{todo.title}}</h3>
+            <TodoItem />
         </div>
     </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
+
 export default {
     name: "Todos",
+    components: {
+        TodoItem
+    },
     // get the data from another component
     props: ["todos"]
 }
