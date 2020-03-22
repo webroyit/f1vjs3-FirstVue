e40143527d9b715dd8b889e8dd2aa88fa18d0e47
royweb <xytank@gmail.com>
2020-03-22 15:28:11 -0400

    Use v-for to loop through array to display the data

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <h1>Todos</h1>
+        <!-- v-bind to add key -->
+        <!-- v-for to loop through array  -->
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <h3>{{todo.title}}</h3>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Todos"
+    name: "Todos",
+    // get the data from another component
+    props: ["todos"]
 }
 </script>
 
