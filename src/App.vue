c73f689059aba9d76a220417d4b23a28cc801ea8
royweb <xytank@gmail.com>
2020-03-22 14:48:56 -0400

    Use data() to store values

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <div id="app">
-    <h1>First Vue</h1>
+    <Todos />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    
+    Todos
+  },
+
+  // store values
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "Buy water",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Clean disk",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Brush teeth",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
