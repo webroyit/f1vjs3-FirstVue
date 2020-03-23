31b76f20fb526043fc18d7d846f8138c8fdf1b10
royweb <xytank@gmail.com>
2020-03-23 11:32:12 -0400

    Use conditional operator to cross out the item if completed is true

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="todo-item">
+    <!-- apply is-complete style if todo.completed is true -->
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
         <p>{{todo.title}}</p>
     </div>
 </template>
 
 <script>
-
 export default {
     name: "TodoItem",
     props: ["todo"]
@@ -17,5 +17,9 @@ export default {
         background-color: #a7ce9e;
         padding: 10px;
         border-bottom: 1px white dotted;
+    }
+
+    .is-complete{
+        text-decoration: line-through;
     }
 </style>
