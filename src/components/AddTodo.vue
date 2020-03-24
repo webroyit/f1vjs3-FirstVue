ebca0f112abf137a7c22ec5a96aff11f8f765bbd
royweb <xytank@gmail.com>
2020-03-24 13:39:35 -0400

    Add a method to create a todo object

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <form>
+        <!-- @submit fire the method when the button is clicked -->
+        <form @submit="addTodo">
             <!-- v-model to create two way data binding -->
             <input type="text" v-model="title" name="title" placeholder="Add what to do" />
             <input type="submit" value="Add" class="btn" />
@@ -9,11 +10,27 @@
 </template>
 
 <script>
+import uuid from "uuid";
+
 export default {
     name: "AddTodo",
     data(){
         return{
             title: ""
+        }
+    },
+    methods:{
+        // create todo object
+        addTodo(){
+            const newTodo = {
+                // generate random id
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+
+            // pass them to the parent component
+            this.$emit("add-todo", newTodo);
         }
     }
 }
